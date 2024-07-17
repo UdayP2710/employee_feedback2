f125fbaaddbcf5aabaabc4b36a8e270694ccfb1e
UdayP2710 <153024688+UdayP2710@users.noreply.github.com>
2024-07-17 11:33:32 +0530

    Update mongodb.js

--- backend/db_config/mongodb.js
+++ backend/db_config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectToDataBase = async () => {
   try {
     console.log("Connecting to database.....");
-    await mongoose.connect("mongodb://localhost:27017/Employee_Review_System");
+    await mongoose.connect("mongodb+srv://udaypandey20000:czeyzu0CO20IoVUr@cluster0.tlw0kyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Database connected successfully!!!!");
   } catch (err) {
     console.log("something went wrong while connecting to database!!!!!");
